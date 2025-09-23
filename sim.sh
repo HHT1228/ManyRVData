ee116987f395279ef7f2d8cbeec324ad0dceac0e
HHT1228 <skyhung2011@hotmail.com>
2025-09-23 16:09:08 +0200

    simulation script update

--- sim.sh
+++ sim.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-fdotp-32b_M1024
+# If no argument is provided
+if [[ $# -lt 1 ]]; then
+    ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-fdotp-32b_M1024
+fi
+
+# Take the first argument as the command
+cmd="$1"
+shift  # shift so $@ now contains only extra args for the command
+
+case "$cmd" in
+    fdotp_small)
+        ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-fdotp-32b_M1024
+        ;;
+    fdotp_large)
+        ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-fdotp-32b_M8192
+        ;;
+    fdotp_xlarge)
+        ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-fdotp-32b_M32768
+        ;;
+    idopt_small)
+        ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-idotp-32b_M1024
+        ;;
+    idopt_xlarge)
+        ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-idotp-32b_M32768
+        ;;
+    ls)
+        ./sim/bin/cachepool_cluster.vsim.gui ./software/build/CachePoolTests/test-cachepool-load-store_M16
+        ;;
+    *)
+        echo "Unknown command: $cmd"
+        ;;
+esac
